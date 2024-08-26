1bf98e374c22b07e2a8f58e6e38a0da5ac708a52
MohamedMohamed <m01126165016@icloud.com>
2024-08-26 06:43:58 +0300

    Import fonts

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,10 @@ function App() {
 
   return (
     <Fragment>
-      <div>
-        Hello World !!
+      <div style={{ height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <p style={{ lineHeight: '100px', letterSpacing: '10px', textAlign: 'center', fontSize: '48px', color: 'white' }}>
+          Hello World !!<br /><span style={{ color: 'wheat', fontSize: '62px' }}>MAIZ</span> Project Under Development
+        </p>
       </div>
     </Fragment>
   )
