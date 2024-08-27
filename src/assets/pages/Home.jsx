ccc082247f81f692b78e61a598429d9648d8db2a
MohamedMohamed <m01126165016@icloud.com>
2024-08-27 08:04:08 +0300

    Finish Second Section

--- src/assets/pages/Home.jsx
+++ src/assets/pages/Home.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import style from '../style/Home.module.css'
 import MainDish from '../img/imageHead.png'
 import Circle from '../img/CircleIcon.png'
 import Star from '../img/ImgIcon.png'
+import ImgOne from '../img/imageOne.png'
+import ImgTwo from '../img/imageTwo.png'
+import ImgThree from '../img/imageThree.png'
+import ImgFour from '../img/imageFour.png'
+import Diamond from '../img/DiamondShape.png'
+import Circles from '../img/Circles.png'
 
 export default function Home() {
     return (
-        <div>
+        <Fragment>
+            {/* First Section */}
             <img src={Star} alt='Main' className={style.floweIcon} />
             <div className={style.imageContainer}>
                 <img src={MainDish} alt='Main' className={style.Image} />
@@ -19,6 +26,29 @@ export default function Home() {
                 </div>
             </div>
             <img src={Circle} alt='Main' className={style.circleIcon} />
-        </div>
+            {/* Second Section */}
+            <div className={style.secSection}>
+                <span>
+                    <img src={ImgOne} alt='Main' className={style.imgOne} />
+                    <img src={Circles} alt='Main' className={style.circlesShape} />
+                </span>
+                <div className={style.subContainer}>
+                    <span className={style.firstText}>A distinctive dining destination<br /> inspired by the culture.</span>
+                    <div className={style.secText}>Experience Al Balad old town of Jeddah vibes. </div>
+                    <button className={style.buttonVenue}>Our Venue</button>
+                    <div className={style.imgesContainer}>
+                        <img src={ImgTwo} alt='Main' />
+                        <img src={ImgThree} alt='Main' />
+                        <img src={ImgFour} alt='Main' />
+                    </div>
+                    <hr className={style.breakLine} />
+                </div>
+                <div className={style.diamondShape}>
+                    <img src={Diamond} alt='Main' />
+                </div>
+            </div>
+            {/* Third Section */}
+            {/* Fourth Section */}
+        </Fragment>
     )
 }
