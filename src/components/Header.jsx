e86b421f1156f29b95b3e8700937b62418159362
MohamedMohamed <m01126165016@icloud.com>
2024-08-29 04:26:45 +0300

    Responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,27 @@
-import React from 'react'
-import style from '../assets/style/Header.module.css'
-import Logo from '../assets/img/Logo.png'
-import ButtonIcon from '../assets/img/ColorButtonIcon.svg'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import style from '../assets/style/Header.module.css';
+import Logo from '../assets/img/Logo.png';
+import ButtonIcon from '../assets/img/ColorButtonIcon.svg';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
         <nav className={style.container}>
             <Link to="/MAIZ/" data-aos="zoom-in">
                 <img className={style.logoImg} src={Logo} alt='Logo' />
             </Link>
-            <ul className={style.list}>
+            <div className={style.burgerMenu} onClick={toggleMenu}>
+                <div className={style.burgerBar}></div>
+                <div className={style.burgerBar}></div>
+                <div className={style.burgerBar}></div>
+            </div>
+            <ul className={`${style.list} ${isOpen ? style.showMenu : ''}`}>
                 <li data-aos="fade-right"><Link to="/MAIZ/about" className={style.navLink}>About</Link></li>
                 <li data-aos="fade-right"><Link to="/MAIZ/menu" className={style.navLink}>Menu</Link></li>
                 <li data-aos="fade-right"><Link to="/MAIZ/venue" className={style.navLink}>Venue</Link></li>
@@ -21,5 +32,5 @@ export default function Header() {
                 <img src={ButtonIcon} alt='Button Icon' />
             </span>
         </nav>
-    )
+    );
 }
